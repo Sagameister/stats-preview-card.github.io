19910d2ac7719fe32b27dbf15f06f44f1e7937e0
Sagameister <86540738+Sagameister@users.noreply.github.com>
2022-02-10 17:12:08 +0100

    adjust animation timing

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ function init() {
 	
 	let tl = gsap.timeline({delay: 1, defaults:{duration:0.8, ease:"Power4.easeOut"}})
         tl.from(".main-wrapper", {width:0, height:"auto", duration:2})
-        .from(".content-wrapper h1", {opacity:0,y:30, duration:1}, "<60%")
+        .from(".img-wrapper", {opacity:0, duration:5}, "<10%")
+        .from(".content-wrapper h1", {opacity:0,y:30, duration:1}, "<20%")
         .from(".content-wrapper p", {opacity:0,y:30, duration:1}, "<20%")
         .from(".stat-wrapper", {opacity:0,y:30, duration:1}, "<20%")
+        .from("stat", {opacity:0,y:30, stagger:1}, "<20%")
 }
 
 window.addEventListener('load', init);
