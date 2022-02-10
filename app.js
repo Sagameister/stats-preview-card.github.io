612584120e5ac665eafaa3e1130710770a9ff355
Sagameister <86540738+Sagameister@users.noreply.github.com>
2022-02-10 17:04:44 +0100

    add gsap codes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // GSAP
 
+
+function init() {
+	gsap.set(".main-wrapper", {autoAlpha:1})
+	
+	
+	let tl = gsap.timeline({delay: 1, defaults:{duration:0.8, ease:"Power4.easeOut"}})
+        tl.from(".main-wrapper", {width:0, height:"auto", duration:2})
+        .from(".content-wrapper h1", {opacity:0,y:30, duration:1}, "<60%")
+        .from(".content-wrapper p", {opacity:0,y:30, duration:1}, "<20%")
+        .from(".stat-wrapper", {opacity:0,y:30, duration:1}, "<20%")
+}
+
+window.addEventListener('load', init);
